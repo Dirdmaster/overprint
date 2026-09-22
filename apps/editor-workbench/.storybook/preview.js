4f00d79d801eca258ec9b876750af2476e793dda
Kevin Aguilar <kevin.aguilar@me.com>
2026-09-22 22:44:08 +0200

    docs: show framework logos in the Storybook selector

--- apps/editor-workbench/.storybook/preview.js
+++ apps/editor-workbench/.storybook/preview.js
@@ -2,18 +2,7 @@ export default {
   globalTypes: {
     framework: {
       description: 'Framework used by component previews and code examples',
-      toolbar: {
-        title: 'Framework',
-        icon: 'component',
-        dynamicTitle: true,
-        items: [
-          { value: 'react', title: 'React' },
-          { value: 'vue', title: 'Vue' },
-          { value: 'next', title: 'Next.js' },
-          { value: 'nuxt', title: 'Nuxt' },
-          { value: 'javascript', title: 'JavaScript' },
-        ],
-      },
+
     },
   },
   initialGlobals: { framework: 'next' },
